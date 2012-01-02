bfaa398b421781598192162736108151a3fc9fa4
Oliver Drobnik <oliver@drobnik.com>
2012-01-02 10:20:36 +0100

    added aggregator

---
+++ Core/Source/DTLocalizableStringAggregator.h
@@ -0,0 +1,13 @@
+//
+//  DTLocalizableStringAggregator.h
+//  genstrings2
+//
+//  Created by Oliver Drobnik on 02.01.12.
+//  Copyright (c) 2012 Drobnik KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTLocalizableStringAggregator : NSObject
+
+@end
---
+++ Core/Source/DTLocalizableStringAggregator.m
@@ -0,0 +1,13 @@
+//
+//  DTLocalizableStringAggregator.m
+//  genstrings2
+//
+//  Created by Oliver Drobnik on 02.01.12.
+//  Copyright (c) 2012 Drobnik KG. All rights reserved.
+//
+
+#import "DTLocalizableStringAggregator.h"
+
+@implementation DTLocalizableStringAggregator
+
+@end
