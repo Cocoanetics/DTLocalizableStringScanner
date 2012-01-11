f5c3efb66fcfdcc478e06b9afa55eaf0d2de714b
Oliver Drobnik <oliver@drobnik.com>
2012-01-11 11:36:59 +0100

    Added test cases

---
+++ Test/Testcases.m
@@ -0,0 +1,32 @@
+
+NSLocalizedString(@"Accounts", nil);
+
+
+// from Adium
+// note: AILocalizedString replaced with NSLocalizedString
+        [tableView_accountList accessibilitySetOverrideValue:NSLocalizedString(@"Accounts", nil)
+                                                                                        forAttribute:NSAccessibilityRoleDescriptionAttribute];
+
+#define CONTACT_NAME_MENU_TITLE         NSLocalizedString(@"Contact Name Format",nil)
+#define ALIAS                                           NSLocalizedString(@"Alias",nil)
+#define ALIAS_SCREENNAME                        NSLocalizedString(@"Alias (User Name)",nil)
+#define SCREENNAME_ALIAS                        NSLocalizedString(@"User Name (Alias)",nil)
+#define SCREENNAME                                      NSLocalizedString(@"User Name",nil)
+
+                                [textField_horizontalWidthText setLocalizedString:NSLocalizedString(@"Maximum Width:",nil)];
+
+        menuItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedStringFromTableInBundle(@"Authorization Requests",nil, [NSBundle bundleForClass:[AIAuthorizationRequestsWindowController class]], nil)
+                                                                                  target:self
+                                                                                  action:@selector(openAuthorizationWindow:)
+                                                                   keyEquivalent:@""];
+
+        NSString *errMsg = NSLocalizedStringWithDefaultValue(@"AsyncSocketCFSocketError",
+                                                                                                                 @"AsyncSocket", [NSBundle mainBundle],
+                                                                                                                 @"General CFSocket error", nil);
+        
+        NSDictionary *info = [NSDictionary dictionaryWithObject:errMsg forKey:NSLocalizedDescriptionKey];
+
+
+                                if ([[menuItem title] isEqualToString:NSLocalizedStringFromTableInBundle(@"Open Link", nil, [NSBundle bundleForClass:[WebView class]], nil)])
+                                        [webViewMenuItems removeObjectIdenticalTo:menuItem];                                    
+                        }
