99b6dea110fe8056c65647e5a0c8e3342c7f2c8b
Oliver Drobnik <oliver@drobnik.com>
2012-01-13 17:04:56 +0100

    Added Test

---
+++ UnitTest/UnitTest.h
@@ -0,0 +1,13 @@
+//
+//  UnitTest.h
+//  UnitTest
+//
+//  Created by Oliver Drobnik on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTest : SenTestCase
+
+@end
---
+++ UnitTest/UnitTest.m
@@ -0,0 +1,35 @@
+//
+//  UnitTest.m
+//  UnitTest
+//
+//  Created by Oliver Drobnik on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UnitTest.h"
+
+@implementation UnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTest");
+}
+
+
+
+
+@end
