706a90a14477cbbbee4e2273741b9050b406cca3
gugmaster <stefan@cocoanetics.com>
2013-02-28 15:09:48 +0100

    Added parser for strings files

---
+++ Core/Source/DTLocalizableStringsParser.m
@@ -0,0 +1,13 @@
+//
+//  DTLocalizableStringsParser.m
+//  genstrings2
+//
+//  Created by Stefan Gugarel on 2/27/13.
+//  Copyright (c) 2013 Drobnik KG. All rights reserved.
+//
+
+#import "DTLocalizableStringsParser.h"
+
+@implementation DTLocalizableStringsParser
+
+@end
---
+++ Test/DTLocalizableStringsParserTest.h
@@ -0,0 +1,13 @@
+//
+//  DTLocalizableStringsParserTest.h
+//  genstrings2
+//
+//  Created by Stefan Gugarel on 2/28/13.
+//  Copyright (c) 2013 Drobnik KG. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DTLocalizableStringsParserTest : SenTestCase
+
+@end
